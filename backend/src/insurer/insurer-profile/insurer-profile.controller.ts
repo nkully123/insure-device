79a253493a3d466a7c023c3dba3f71f4333bf6b8
Thualani Sikhosana <sikhosana@thedigitalacademy.co.za>
2021-07-26 14:31:47 +0200

    insurer-profile

--- backend/src/insurer/insurer-profile/insurer-profile.controller.ts
+++ backend/src/insurer/insurer-profile/insurer-profile.controller.ts
@@ -1,4 +1,66 @@
-import { Controller } from '@nestjs/common';
+import {Controller,Post,Body, Get, Param, Patch, Delete} from '@nestjs/common';
+import { InsurerProfileService } from './insurer-profile.service';
 
 @Controller('insurer-profile')
-export class InsurerProfileController {}
+export class InsurerProfileController 
+{
+    constructor(private readonly insurerProfile: InsurerProfileService){}
+
+    @Post()
+    async addInsuranceCompany
+    (
+        @Body('insuranceName') insusrerName:string,
+        @Body('email') insusrerEmail:string, 
+        @Body('contactNo') insusrerContactNo:string, 
+        @Body('aboutCompany') insusrerAboutCompany:string,
+        @Body('imageURL') insusrerImageURL:string,
+         
+    ) {
+
+        
+
+      const genID = await this.insurerProfile.insertInsurance
+      (
+        insusrerName,
+        insusrerEmail,
+        insusrerContactNo,
+        insusrerAboutCompany,
+        insusrerImageURL
+        );
+
+      return "company ID : " + genID;
+    }
+
+ 
+
+    @Delete(':id')
+    removeInsuranceCompany(@Param('id') insuranceId: string)
+    {
+         return this.insurerProfile.deleteInsurenceCompany(insuranceId);
+
+    }
+
+    @Patch(':id')
+    async updateUser(
+     @Param('id') insuranceID:string,
+     @Body('insuranceName') insusrerName:string,
+     @Body('email') insusrerEmail:string, 
+     @Body('contactNo') insusrerContactNo:string, 
+     @Body('aboutCompany') insusrerAboutCompany:string,
+     @Body('imageURL') insusrerImageURL:string,
+     ){
+       await  this.insurerProfile.updateInsuranceCompany
+       (
+        insuranceID,
+        insusrerName,
+        insusrerEmail,
+        insusrerContactNo,
+        insusrerAboutCompany,
+        insusrerImageURL
+       )
+         console.log(insuranceID);
+ 
+         return "Insurance Company ID " + insuranceID + " was updated"
+     }
+
+}
